71e22e73c2d2d33fd07b2c9fc4d92551ebfc7ea2
JJ Driscoll <james@jjdriscoll.com>
2021-11-30 21:56:20 -0500

    updated layout and features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,8 +10,14 @@ import { AppService } from './app.service';
 })
 export class AppComponent {
   inputProvider = 'Coingecko'
+  inputMinPrice = 'Any'
   inputMaxPrice = '.000001'
   inputMinVolume = '100000'
+  inputMaxVolume = 'Any'
+  inputInclude = ''
+  inputExclude = 'Inu'
+  inputSortBy = 'volume'
+  inputSortDirection = 'asc'
   disableInput = false
   error = false
   coins: any
@@ -19,40 +25,40 @@ export class AppComponent {
 
   constructor(private appService: AppService) { }
 
-  ngOnInit() {
+  async ngOnInit() {
     const localStorage_coingecko = localStorage.getItem('coingecko');
+
     if (localStorage_coingecko) {
       this.coins = JSON.parse(localStorage_coingecko)
-      console.log(this.coins);
+      console.log('[CoinGecko] cached data:', this.coins);
+      // this.results = this.coins
+      await this.fetchData(true)
+
     }
   }
 
-  async search(useCache: boolean) {
-    this.results = undefined;
-
+  async fetchData(useCache: boolean) {
     try {
       this.disableInput = true
-      let searchResponse: any;
+      // let searchResponse: any;
 
       if (this.coins && useCache) {
-        searchResponse = this.coins
+
         await new Promise((resolve: any, reject: any) => {
           try {
             setTimeout(function () {
               resolve(true)
-            }, 750);
-          } catch(error) {
+            }, 666);
+          } catch (error) {
             reject(error)
           }
         })
-        this.results = searchResponse
+
       } else {
+
         this.coins = undefined
-        searchResponse = await this.appService.pullData(this.inputProvider, {
-          price_min: null,
-          price_max: this.inputMaxPrice,
-          volume_min: this.inputMinVolume,
-          volume_max: null,
+
+        const searchResponse: any = await this.appService.pullData(this.inputProvider, {
           filter: false,
           unique: true
         })
@@ -60,31 +66,235 @@ export class AppComponent {
           this.error = searchResponse.message
           this.results = undefined
         } else {
-          this.results = searchResponse
           this.coins = searchResponse
-          // localStorage.clear()
+
           localStorage.removeItem('coingecko')
-          localStorage.setItem('coingecko', JSON.stringify(this.results))
+          localStorage.setItem('coingecko', JSON.stringify(this.coins))
         }
+
+      }
+
+      this.results = this.coins
+      if (this.results) {
+        this.filterData(true, true)
+
       }
 
       this.disableInput = false
-      if (searchResponse && this.results) {
-        
-        // console.log(this.coins)
-        this.results = this.results.filter((coin: any) => {
-          return (coin.price <= this.inputMaxPrice && coin.volume >= this.inputMinVolume)
-        })
-        // console.log(this.coins)
-        this.results = this.results.sort((a: any, b: any) => a.volume - b.volume )
-        // console.log(this.coins)
-        return true
-      } else {
-        return false
-      }
     } catch (error) {
       this.disableInput = false
       throw error
     }
   }
+
+  filterData(keywords?: boolean, sort?: boolean) {
+    // console.log('this.inputMinPrice',this.inputMinPrice)
+    // console.log('this.inputMaxPrice',this.inputMaxPrice)
+    // console.log('this.inputMinVolume',this.inputMinVolume)
+    // console.log('this.inputMaxVolume',this.inputMaxVolume)
+
+    this.results = this.coins.filter((coin: any) => {
+      return (
+        coin.price >= Number((this.inputMinPrice == 'Any' ? 0 : this.inputMinPrice) || 0) &&
+        coin.price <= Number((this.inputMaxPrice == 'Any' ? 10000000 : this.inputMaxPrice) || 10000000) &&
+        coin.volume >= Number((this.inputMinVolume == 'Any' ? 0 : this.inputMinVolume) || 0) &&
+        coin.volume <= Number((this.inputMaxVolume == 'Any' ? 1000000000000000 : this.inputMaxVolume) || 1000000000000000)
+      )
+    })
+    // console.log(this.results)
+    if (keywords) this.filterKeywordsData(true, true)
+    // this.filterKeywordsData('inputInclude')
+    // this.filterKeywordsData('inputExclude')
+    if (sort) this.sortData()
+  }
+
+  filterIncludeKeywordsData(event: any) {
+    console.log('this.inputInclude',this.inputInclude)
+    console.log('event',event)
+
+    this.filterData(false, false)
+
+    // await new Promise((resolve: any, reject: any) => {
+    //   try {
+    //     setTimeout(function () {
+    //       resolve(true)
+    //     }, 100);
+    //   } catch (error) {
+    //     reject(error)
+    //   }
+    // })
+
+    if (event.length) {
+      this.results = this.results.filter((coin: any) => {
+        let includes = false;
+        (event || '').split(' ').forEach((str) => {
+          includes = coin.name.includes(str)
+        })
+        return includes
+      })
+    }
+
+    this.filterKeywordsData(false, true)
+
+    // this.filterExcludeKeywordsData(this.inputExclude)
+    this.sortData()
+  }
+
+  filterExcludeKeywordsData(event: any) {
+    console.log('this.inputExclude',this.inputExclude)
+    console.log('event',event)
+
+    this.filterData(false, false)
+
+    // await new Promise((resolve: any, reject: any) => {
+    //   try {
+    //     setTimeout(function () {
+    //       resolve(true)
+    //     }, 100);
+    //   } catch (error) {
+    //     reject(error)
+    //   }
+    // })
+
+
+    if (event.length) {
+      this.results = this.results.filter((coin: any) => {
+        let includes = false;
+        (event || '').split(' ').forEach((str) => {
+          includes = coin.name.includes(str)
+        })
+        return !includes
+      })
+    }
+
+    this.filterKeywordsData(true, false)
+
+    // this.filterIncludeKeywordsData(this.inputInclude)
+    this.sortData()
+  }
+
+  filterKeywordsData(inputInclude: boolean, inputExclude: boolean) {
+    // console.log('this.inputInclude',this.inputInclude)
+    // console.log('this.inputExclude',this.inputExclude)
+    // console.log('event',event)
+
+    // await new Promise((resolve: any, reject: any) => {
+    //   try {
+    //     setTimeout(function () {
+    //       resolve(true)
+    //     }, 100);
+    //   } catch (error) {
+    //     reject(error)
+    //   }
+    // })
+
+    if (inputInclude && this.inputInclude.length) {
+      this.results = this.results.filter((coin: any) => {
+        let includes = false;
+        // this.inputInclude.split(' ').forEach((str) => {
+          this.inputInclude.split(' ').forEach((str) => {
+          includes = coin.name.includes(str)
+        })
+        return includes
+      })
+    }
+
+    if (inputExclude && this.inputExclude.length) {
+      this.results = this.results.filter((coin: any) => {
+        let includes = false;
+        // this.inputExclude.split(' ').forEach((str) => {
+          this.inputExclude.split(' ').forEach((str) => {
+          includes = coin.name.includes(str)
+        })
+        return !includes
+      })
+    }
+    // console.log(this.results)
+  }
+
+  sortData() {
+    // console.log('this.inputSortDirection',this.inputSortDirection)
+    // console.log('this.inputSortBy',this.inputSortBy)
+
+    if (this.inputSortDirection == 'asc') {
+      this.results = this.results.sort((a: any, b: any) => a[this.inputSortBy] - b[this.inputSortBy])
+    } else {
+      this.results = this.results.sort((a: any, b: any) => b[this.inputSortBy] - a[this.inputSortBy])
+    }
+    // console.log(this.results)
+  }
+
+  refreshCache() {
+    this.coins = undefined
+    this.results = undefined
+
+    localStorage.removeItem('coingecko')
+  }
+
+//   async search(useCache: boolean) {
+//     this.results = undefined;
+
+//     try {
+//       this.disableInput = true
+//       let searchResponse: any;
+
+//       if (this.coins && useCache) {
+//         searchResponse = this.coins
+//         await new Promise((resolve: any, reject: any) => {
+//           try {
+//             setTimeout(function () {
+//               resolve(true)
+//             }, 750);
+//           } catch (error) {
+//             reject(error)
+//           }
+//         })
+//         this.results = searchResponse
+//       } else {
+//         this.coins = undefined
+//         searchResponse = await this.appService.pullData(this.inputProvider, {
+//           price_min: this.inputMinPrice,
+//           price_max: this.inputMaxPrice,
+//           volume_min: this.inputMinVolume,
+//           volume_max: this.inputMaxVolume,
+//           filter: false,
+//           unique: true
+//         })
+//         if (searchResponse && searchResponse.message && searchResponse.error) {
+//           this.error = searchResponse.message
+//           this.results = undefined
+//         } else {
+//           this.results = searchResponse
+//           this.coins = searchResponse
+//           // localStorage.clear()
+//           localStorage.removeItem('coingecko')
+//           localStorage.setItem('coingecko', JSON.stringify(this.results))
+//         }
+//       }
+
+//       this.disableInput = false
+//       if (searchResponse && this.results) {
+
+//         // console.log(this.coins)
+//         this.results = this.results.filter((coin: any) => {
+//           return (
+//             coin.price >= (this.inputMinPrice || 0) &&
+//             coin.price <= (this.inputMaxPrice || 1000000) &&
+//             coin.volume >= (this.inputMinVolume || 0) &&
+//             coin.volume <= (this.inputMaxVolume || 1000000000000000)
+//           )
+//         })
+//         // console.log(this.coins)
+//         this.results = this.results.sort((a: any, b: any) => a.volume - b.volume)
+//         // console.log(this.coins)
+//         return true
+//       } else {
+//         return false
+//       }
+//     } catch (error) {
+//       this.disableInput = false
+//       throw error
+//     }
+//   }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { HttpClientModule } from '@angular/common/http';
+
 import { FormsModule } from '@angular/forms';
+
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatRadioModule } from '@angular/material/radio';
 import { MatIconModule } from '@angular/material/icon';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
@@ -26,6 +32,8 @@ import { AppComponent } from './app.component';
     MatSelectModule,
     MatInputModule,
     MatButtonModule,
+    MatButtonToggleModule,
+    MatRadioModule,
     MatIconModule,
     MatProgressSpinnerModule,
     MatProgressBarModule,
